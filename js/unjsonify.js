62492e4ad9e6e83d659623bddb8e8b2483c502ac
Administrator <admin@local.host>
2014-07-19 00:45:54 +0100

    Gradual Improvements, tomorrow will re-write to be more semantic and utilise jquery to make life easier

--- js/unjsonify.js
+++ js/unjsonify.js
@@ -1,13 +1,17 @@
+var unjsonify = (function() {
 /*
 *   Variables
 */
 var count = 0;
+var options = {
+    jump: false
+}
 
 /*
 *   Constructors
 */
 var Node = function(href, node, val, tree, count) {
-    var node = "<li><a class='"+"node "+node+count+" expandable' name='"+href.replace('#','')+"' href='"+href+"'>"+node+":"+val+"</a><div class='expandable' id='"+href+"'>"+parseTree(val)+"</div></li>";
+    var node = "<li id='" + count + "'><a title='Node' class='" /* + node*/ + "node " + "expandable' name='"+  ((options.jump == true) ? href.replace('#','') : "")  + "' href='" + href + "'>" + node + "</a>" + ":  " + "<b id='type' title='type'>" + typeof val + "</b>" + "<div class='expandable' id='" + href + "'>" + parseTree(val) + "</div></li>";
     return node; 
 }
        
@@ -24,9 +28,10 @@ function parseTree(nodes) {
         	    var newNode = Node("#"+genKey(8), node, val, tree, count);
         	    tree += newNode;           
         	} else {
-                    tree += "<li id="+count+">"+node+":"+"<p id='prop'>"+val+"</p>"+"</li>";
+                tree += "<li id="+count+">" + "<p id='ref' title='Node'> " + node + "</p>" + ":  " + "<p id='prop' title='Property'>" + val + "</p>" + "<b id='type' title='Type'>" + "   :   " + typeof val + "</b>" + "</li>";
         	}     
     }
+
     return tree+"</ul>"
 }
 
@@ -37,8 +42,18 @@ function genKey(s) {
 /*
 *   Function
 */
-function unjsonify(input, callback) {
-    var htmlOut = parseTree(JSON.parse(input));
-    
+function unjsonify(input, params, callback) {
+    //Reset Count
+    count = 0;
+    //Set params, if any.
+    if (typeof params === "object") options = params;
+    //Begin Recursion
+    var htmlOut = "<div class='nodes'>"+parseTree(JSON.parse(input))+"</div>";  
+    //Send user back their lovely html.
     callback(htmlOut);
 }
+
+
+return unjsonify;
+
+})();
